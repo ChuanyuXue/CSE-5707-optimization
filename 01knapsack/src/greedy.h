2f7a0239b787949044d240d1df4191425cea40e6
Chuanyu Xue <CS_xcy@126.com>
2022-09-11 10:32:10 -0400

    update framework

---
+++ 01knapsack/src/greedy.h
@@ -0,0 +1,39 @@
+const int 
+
+partition (arr[], low, high)
+
+{
+
+    // pivot (Element to be placed at right position)
+pivot = arr[high];  
+
+ i = (low – 1)  // Index of smaller element and indicates the 
+// right position of pivot found so far
+
+for (j = low; j <= high- 1; j++){
+
+ // If current element is smaller than the pivot
+    if (arr[j] < pivot){
+    i++;    // increment index of smaller element
+    swap arr[i] and arr[j]
+        }
+    }
+
+    swap arr[i + 1] and arr[high])
+    return (i + 1)
+}
+
+void quick_sort(int* array){
+
+}
+
+int* optimize(int** validation, const int n){
+    int result[n];
+    int rate[n];
+    for(i = 0; i++; i < n){
+        rate[i] = validation[i][1] /  validation[i][0]
+    }
+
+
+    
+}
