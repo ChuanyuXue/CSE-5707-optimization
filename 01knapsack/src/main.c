c85b6013db0ac6b3f5e3c7e3a2ade34ea61ff058
Chuanyu Xue <CS_xcy@126.com>
2022-09-11 11:51:43 -0400

    update greedy algorithm

--- 01knapsack/src/main.c
+++ 01knapsack/src/main.c
@@ -1,30 +1,31 @@
+#include "greedy.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 const int INPUT_COLUMN = 2;
 const int INPUT_ROW_MAX = 65536;
 
-int main(int argc, char *argv[]){
-    FILE *file;
+int main(int argc, char *argv[]) {
+  FILE *file;
 
-    int validation[INPUT_COLUMN][INPUT_ROW_MAX];
-    int weight;
-    int value;
+  int validation[INPUT_ROW_MAX][INPUT_COLUMN];
+  int weight;
+  int value;
+  int opt_est;
 
-    // IO
-    int row_count;
-    // file = fopen("../data/low-dimensional/f1_l-d_kp_10_269", "r");
-    file = fopen(argv[1], "r");
-    row_count = 0;
-    while (!feof (file))
-    {  
-        fscanf (file, "%d %d", &weight, &value);    
-        validation[row_count][0] = weight;
-        validation[row_count][1] = value;
-        // printf("%d %d", validation[row_count][0],validation[row_count][1]);
-    }
-    fclose (file);        
+  // IO
+  int row_count;
+  // file = fopen("../data/low-dimensional/f1_l-d_kp_10_269", "r");
+  file = fopen(argv[1], "r");
+  row_count = 0;
+  while (!feof(file)) {
+    fscanf(file, "%d %d", &weight, &value);
+    validation[row_count][0] = weight;
+    validation[row_count][1] = value;
+    printf("%d %d", validation[row_count][0], validation[row_count][1]);
+    row_count++;
+  }
+  fclose(file);
 
-
-
+  opt_est = optimize(validation, row_count);
 }
